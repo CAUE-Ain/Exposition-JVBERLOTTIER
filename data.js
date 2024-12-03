c07c93f87481ac05bf1a03f86ed1332606ffa3fa
CAUE-Ain <lmaizeray@caue-ain.com>
2024-12-03 16:45:56 +0100

    Upload 360

---
+++ data.js
@@ -0,0 +1,149 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-pinsard_-ars_vue_ext_360",
+      "name": "PINSARD_ Ars_Vue_ext_360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.4083983627516938
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9493695387906964,
+          "pitch": 0.07649762389558923,
+          "rotation": 0,
+          "target": "1-pinsard_-ars_vue_int_360"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-pinsard_-ars_vue_int_360",
+      "name": "PINSARD_ Ars_Vue_int_360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.8641285751487775,
+        "pitch": 0.0761809544937666,
+        "fov": 1.4083983627516938
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8874971786067505,
+          "pitch": -0.09835166358531744,
+          "rotation": 0,
+          "target": "0-pinsard_-ars_vue_ext_360"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-pinsard_-bourg_vue_ext_360",
+      "name": "PINSARD_ Bourg_Vue_ext_360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-pinsard_-bourg_vue_int_360",
+      "name": "PINSARD_ Bourg_Vue_int_360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "JVBerlottier - Visite virtuelle",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
